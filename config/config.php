c7b92a160c48a6683ca3c8a2e784fa05e0c98ba7
Edgar Apaza <edgarapazac@gmail.com>
2024-01-27 17:47:57 -0500

    Iniciando la pagina Web para el IEP San Luis Ilo

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('URL', 'http://localhost/iepsanluis/');
+
+define('COMPANY', 'IEP San Luis');
+define('HOST', 'localhost');
+define('USER', 'usuario');
+define('DB', 'katari');
+define('PASSWORD', 'archivo123$');
+define('CHATSET', 'utf8');
+
+date_default_timezone_set('America/Lima');
---
+++ views/main/index.php
@@ -0,0 +1,12 @@
+<?php require('views/header.php'); ?>
+
+<div class="grid-container">
+  <div class="grid-x">
+    <div class="cell">
+      menu
+    </div>
+  </div>
+</div>
+
+
+<?php require('views/footer.php'); ?>
